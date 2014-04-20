cf461ef554292239821637050cee59a340193085
xLink <xLink@cybershade.org>
2014-04-20 22:23:47 +0100

    upgraded the config set

---
+++ config/app.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'themes' => array(
+        'frontend' => 'default',
+        'backend'  => 'default-admin',
+    )
+);
--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,8 @@ return array(
     'paths' => array(
         'admin' => 'admin',
         'api'   => 'api',
+
+        'user'  => 'user',
     ),
 
 );
