0e53e40f3b1fb9027c3e8421686a97eb98c3687a
Dan Aldridge <xLink@cybershade.org>
2014-07-21 14:46:37 +0100

    made it not re-publish the dependency config if its already there

--- commands/BaseCommand.php
+++ commands/BaseCommand.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Console\Command;
+use File;
 
 class BaseCommand extends Command
 {
@@ -53,8 +54,8 @@ class BaseCommand extends Command
 
     }
 
-    public function install(Array $packages)
-   {
+    public function install(array $packages)
+    {
         foreach ($packages as $pkg => $settings) {
             if (array_get($settings, 'migrate', false) == true && !empty($pkg)) {
                 $this->comment(sprintf('Migrating %s Package...', array_get($settings, 'name', '')));
@@ -66,9 +67,11 @@ class BaseCommand extends Command
                 $this->call('db:seed', array('--class' => array_get($settings, 'seedclass')));
             }
 
-            if (array_get($settings, 'config', false) == true && !empty($pkg)) {
+            if (array_get($settings, 'config', false) == true && !empty($pkg) && !File::exists(base_path().'/app/config/packages/'.$pkg)) {
                 $this->comment(sprintf('Publishing %s Config...', array_get($settings, 'name', '')));
                 $this->call('config:publish', array('package' => $pkg));
+            } else {
+                $this->comment(sprintf('Publishing %s Config... config already published', array_get($settings, 'name', '')));
             }
         }
     }
