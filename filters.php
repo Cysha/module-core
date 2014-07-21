0ecafac04b1f12fd0ad1699f64032ac531fe8a01
Dan Aldridge <xLink@cybershade.org>
2014-07-21 14:46:56 +0100

    fixed the alpha_spaces validation rule

--- filters.php
+++ filters.php
@@ -90,7 +90,7 @@
 |--------------------------------------------------------------------------
 */
     Validator::extend('alpha_spaces', function ($attribute, $value) {
-        return preg_match('/^([-a-z0-9_-\s])+$/i', $value);
+        return preg_match('/^[\pL\s]+$/u', $value);
     });
 
 /*
