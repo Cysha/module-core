ff92bdc315afd418dbd35d66b5a3b97730d6d14e
Dan Aldridge <xLink@cybershade.org>
2014-07-22 13:05:59 +0100

    added secure key generation to the install procedure

--- controllers/BaseAdminController.php
+++ controllers/BaseAdminController.php
@@ -84,14 +84,24 @@ class BaseAdminController extends BaseController
             // process any acp_menus this module might have
             if (Config::has($name.'::admin.acp_menu')) {
                 foreach (Config::get($name.'::admin.acp_menu') as $section => $menu) {
-                    $acp[$section] = isset($acp[$section]) ? array_merge($acp[$section], $menu) : $menu;
+                    if (is_array($menu)) {
+                        $acp[$section] = array_merge($acp[$section], $menu);
+                        continue;
+                    }
+
+                    $acp[$section] = $menu;
                 }
             }
 
             // process any config menus this module might have
             if (Config::has($name.'::admin.config_menu')) {
                 foreach (Config::get($name.'::admin.config_menu') as $section => $menu) {
-                    $inline[$section] = isset($inline[$section]) ? array_merge($inline[$section], $menu) : $menu;
+                    if (is_array($menu)) {
+                        $inline[$section] = array_merge($inline[$section], $menu);
+                        continue;
+                    }
+
+                    $inline[$section] = $menu;
                 }
             }
 
