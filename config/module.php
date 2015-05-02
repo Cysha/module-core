e8cf2d5d6e15089cf8c5804bfa7cf3b0e6b9f97e
xLink <xLink@cybershade.org>
2015-05-02 14:32:51 +0100

    started the push to l5

--- config/module.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    'name'        => 'Core Module',
-    'author'      => 'xLink (Dan Aldridge)',
-
-    'date-format' => 'd-m-Y h:i:s',
-    'pxcms-index' => 'Cysha\Modules\Core\Controllers\Module\ExtrasController@getHomepage',
-);
--- routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$namespace = 'Cysha\Modules\Core\Controllers';
-
-require 'routes-admin.php';
-require 'routes-api.php';
-require 'routes-module.php';
