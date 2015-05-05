14247f2855785e752f676ad2d278227c07ddfbca
xLink <xLink@cybershade.org>
2015-05-05 22:19:27 +0100

    temp remove the config dir

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'name'        => 'Core Module',
-    'author'      => 'xLink (Dan Aldridge)',
-
-    'date-format' => 'd-m-Y h:i:s',
-    'pxcms-index' => 'Cms\Modules\Core\Http\Controllers\Module\PagesController@getHomepage',
-];
