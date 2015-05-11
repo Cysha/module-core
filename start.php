d13f0f7f639917c000bad19465067ac00a937d34
xLink <xLink@cybershade.org>
2015-05-11 15:33:19 +0100

    updated to use the new routingprovider

--- start.php
+++ start.php
@@ -1,9 +1,1 @@
 <?php
-
-$module = last(explode(DIRECTORY_SEPARATOR, __DIR__));
-$namespace = sprintf('\Cms\Modules\%s\Http\Controllers', $module);
-
-require __DIR__ . '/Http/routes-frontend.php';
-require __DIR__ . '/Http/routes-backend.php';
-require __DIR__ . '/Http/routes-api.php';
-// require __DIR__ . '/composers.php';
