a049bb71f3d6d605d7d14cb41c83874be59e4b23
Dan Aldridge <xLink@cybershade.org>
2015-05-12 17:00:40 +0100

    removed some useless info now being served in module.json

--- Config/config.php
+++ Config/config.php
@@ -1,9 +1,6 @@
 <?php
 
 return [
-    'name'        => 'Core Module',
-    'author'      => 'xLink (Dan Aldridge)',
-
     'date-format' => 'd-m-Y h:i:s',
     'pxcms-index' => 'Cms\Modules\Core\Http\Controllers\Module\PagesController@getHomepage',
 ];
