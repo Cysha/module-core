91df6728d15d80bb59830c9fe7963823f39da6eb
xLink <xLink@cybershade.org>
2015-05-12 10:33:00 +0100

    added the admin menu setup

--- Http/Controllers/BaseAdminController.php
+++ Http/Controllers/BaseAdminController.php
@@ -2,6 +2,8 @@
 
 use Route;
 use File;
+use Menu;
+use Config;
 
 class BaseAdminController extends BaseController
 {
@@ -30,7 +32,7 @@ class BaseAdminController extends BaseController
 
         // then add the control panel stuff
         $this->addPageAssets();
-        //$this->adminMenu();
+        $this->adminMenu();
     }
 
     public function setActions(array $actions)
@@ -55,4 +57,104 @@ class BaseAdminController extends BaseController
         }
     }
 
+
+    /**
+     * Sets up the admin panel
+     */
+    public function adminMenu()
+    {
+        $this->theme->breadcrumb()->add('ACP Dashboard', route('pxcms.admin.index'));
+
+        // generate the admin menu handler
+        $acp = Menu::handler('acp');
+        $acp->add(route('pxcms.admin.index'), '<i class="fa fa-dashboard"></i> Dashboard');
+
+        // loop through each of the menus, merge them and then process them
+        $acp = array();
+        $inline = array();
+
+        foreach (app('modules')->getOrdered() as $module) {
+            $name = $module->getName();
+echo \Debug::dump(Config::has($name.'::admin.acp_menu'), $name);
+            // process any acp_menus this module might have
+            if (Config::has($name.'::admin.acp_menu')) {
+                foreach (Config::get($name.'::admin.acp_menu') as $section => $menu) {
+                    $acp[$section] = !empty($acp[$section]) ? array_merge($acp[$section], $menu) : $menu;
+                }
+            }
+
+            // process any config menus this module might have
+            if (Config::has($name.'::admin.config_menu')) {
+                foreach (Config::get($name.'::admin.config_menu') as $section => $menu) {
+                    $inline[$section] = !empty($inline[$section]) ? array_merge($inline[$section], $menu) : $menu;
+                }
+            }
+
+        }
+
+        // add the menus to the system
+        $this->processMenu($acp, 'acp');
+        $this->processMenu($inline, 'acp.config_menu');
+    }
+
+    /**
+     * Processes the arrays into a menu set
+     *
+     * @param  array  $menus
+     * @param  string $handler
+     * @return bool
+     */
+    public function processMenu(array $menus, $handler = 'acp')
+    {
+        if (empty($menus)) {
+            return false;
+        }
+
+        $acp = Menu::handler($handler);
+
+        foreach ($menus as $section => $link) {
+            if (empty($link)) {
+                continue;
+            }
+
+            if (!is_array($link) || !count($link)) {
+                $this->addSection($acp, $link, $section);
+                continue;
+            }
+
+            $section = trim(e($section));
+            $s = Menu::items('section-'.$section);
+
+            $children = false;
+            foreach ($link as $url => $anchor) {
+                $this->addSection($s, $anchor, $url);
+                $children = true;
+            }
+
+            if ($children === true) {
+                $acp->add('#', $section, $s);
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks the permissions on the routes
+     *
+     * @param object $menu
+     * @param string $link
+     * @param string $section
+     */
+    public function addSection(&$menu, $link, $section)
+    {
+        if (is_number($section)) {
+            $menu->add('#', $link)->addClass('divider');
+        } else {
+            if (Auth::user()->can($section) === false) {
+                return;
+            }
+            $menu->add(route($section), $link);
+        }
+    }
 }
