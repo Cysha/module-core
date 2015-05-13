94dcf6900c2230a1e4ddbcfe3566e37785ce4580
Dan Aldridge <xLink@cybershade.org>
2015-05-13 10:50:13 +0100

    added dbselect & config macros to the form facade

--- macros.php
+++ macros.php
@@ -1,1 +1,16 @@
 <?php
+
+    Form::macro('DBSelect', function ($id, $collection, $options = ['id' => 'name']) {
+        $key = key($options);
+        $rows = $collection->lists($options[$key], $key);
+
+        return Former::select($id)->options($rows);
+    });
+
+    Form::macro('Config', function ($key, $type = 'text', $default = null) {
+        if (in_array($type, ['radios', 'radio'])) {
+            return Former::$type($key)->check(config($key, $default));
+        }
+
+        return Former::$type($key)->value(config($key, $default));
+    });
