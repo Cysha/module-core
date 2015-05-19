1b5a4cb381cddd700e9ffb9035e3a2257044b3ba
xLink <xLink@cybershade.org>
2015-05-19 22:50:10 +0100

    added some a base repository setup

---
+++ Repositories/BaseRepositoryInterface.php
@@ -0,0 +1,43 @@
+<?php namespace Cms\Modules\Core\Repositories;
+
+interface BaseRepositoryInterface
+{
+    public function getModel();
+
+    public function all();
+
+    public function count();
+
+    public function create(array $data);
+
+    public function createMultiple(array $data);
+
+    public function delete();
+
+    public function deleteById($id);
+
+    public function deleteMultipleById(array $ids);
+
+    public function first();
+
+    public function get();
+
+    public function getById($id);
+
+    public function limit($limit);
+
+    public function orderBy($column, $value);
+
+    public function updateById($id, array $data);
+
+    public function where($column, $value, $operator = '=');
+
+    public function whereIn($column, $value);
+
+    public function with($relations);
+
+    public function paginate($perPage = 0, $columns = ['*']);
+
+    public function transformModels($data);
+
+}
