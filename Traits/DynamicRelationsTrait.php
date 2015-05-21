488c1e342849d9c519c6834397f8d70508bf2925
xLink <xLink@cybershade.org>
2015-05-21 13:43:46 +0100

    added the DynamicRelationsTrait

---
+++ Traits/DynamicRelationsTrait.php
@@ -0,0 +1,54 @@
+<?php namespace Cms\Modules\Core\Traits;
+
+trait DynamicRelationsTrait
+{
+
+    /**
+     * This function will be called when a function is missing on this model
+     *
+     * @param string $method
+     * @param array $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        $currentClass = class_basename($this);
+        // grab the module & model
+        list(,,$currentModule,,$currentModel) = explode('\\', get_class($this));
+
+        $modelConfig = array_column(config('cms'), 'models');
+        // if we dont have anything here, just return
+        if (empty($modelConfig)) {
+            return parent::__call($method, $parameters);
+        }
+
+        $newFunctions = [];
+        // loop over the $modelConfig as $modules
+        foreach ($modelConfig as $modules) {
+            foreach ($modules as $module => $models) {
+                foreach ($models as $model => $functions) {
+                    foreach ($functions as $fname => $function) {
+                        $newFunctions[$model.'_'.$fname] = $function;
+                    }
+                }
+            }
+        }
+
+        // check to see if any of the modules are trying to be clever
+        $function = false;
+        if (array_key_exists($currentClass.'_'.$method, $newFunctions)) {
+            $function = $newFunctions[$currentClass.'_'.$method];
+        }
+
+        if ($function !== false) {
+            $params = [$this];
+            foreach ($parameters as $p) {
+                $params[] = $p;
+            }
+            return call_user_func_array($function, $params);
+        }
+
+        // No relation found, return the call to parent (Eloquent) to handle it.
+        return parent::__call($method, $parameters);
+    }
+}
