a8e8833fbd7e618b3628cd588301446068ea5385
Dan Aldridge <xLink@cybershade.org>
2015-05-26 12:24:01 +0100

    added a comment to the / route

--- Http/routes-frontend.php
+++ Http/routes-frontend.php
@@ -1,4 +1,14 @@
 <?php
 
 $router->get('test', 'PagesController@test');
-$router->get('/', ['as' => 'pxcms.pages.home', 'uses' => config('cms.core.app.pxcms-index', 'PagesController@getHomepage')]);
+
+/**
+ * This setting can be controlled from the admin panel.
+ * In the interest of keeping this dynamic, don't directly overload it,
+ * add a setting to your modules config file.
+ * cms.MODULE.config.pxcms-index
+ */
+$router->get('/', [
+    'as' => 'pxcms.pages.home',
+    'uses' => config('cms.core.app.pxcms-index', 'PagesController@getHomepage')
+]);
