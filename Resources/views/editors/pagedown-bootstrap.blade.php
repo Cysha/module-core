c6de533fe05e677a2b1357faa51494ec5ae0821f
xLink <xLink@cybershade.org>
2015-05-30 20:47:39 +0100

    swapped l4 blade tags for l5

--- Resources/views/editors/pagedown-bootstrap.blade.php
+++ Resources/views/editors/pagedown-bootstrap.blade.php
@@ -1,6 +1,6 @@
 @if (isset($id))
 
-<textarea id="{{ $id }}" name="{{ $id }}" class="form-control" rows="10">{{ $content or '' }}</textarea>
+<textarea id="{{ $id }}" name="{{ $id }}" class="form-control" rows="10">{!! $content or '' !!}</textarea>
 
 <script type="text/javascript">
 (function () {
