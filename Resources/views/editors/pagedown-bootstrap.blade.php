8a019f20adb75086481500666649bfb8077dcaa1
xLink <xLink@cybershade.org>
2015-06-04 12:06:25 +0100

    made the textarea id prefixed with txt_

--- Resources/views/editors/pagedown-bootstrap.blade.php
+++ Resources/views/editors/pagedown-bootstrap.blade.php
@@ -1,10 +1,10 @@
 @if (isset($id))
 
-<textarea id="{{ $id }}" name="{{ $id }}" class="form-control" rows="10">{!! $content or '' !!}</textarea>
+<textarea id="txt_{{ $id }}" name="{{ $id }}" class="form-control" rows="10">{!! $content or '' !!}</textarea>
 
 <script type="text/javascript">
 (function () {
-    jQuery('textarea#{{ $id }}').pagedownBootstrap({
+    jQuery('textarea#txt_{{ $id }}').pagedownBootstrap({
         'sanatize': false
     });
 })();
