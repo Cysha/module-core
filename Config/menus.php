64ddbb12147f2122f6b12f529a45f62af7def509
xLink <xLink@cybershade.org>
2015-06-23 00:10:18 +0100

    added base menus.php

---
+++ Config/menus.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'backend_sidebar' => [
+        [
+            'route'         => 'pxcms.admin.index',
+            'text'          => 'Dashboard',
+            'icon'          => 'fa-dashboard',
+            'order'         => 1,
+        ],
+        'User Management' => [],
+        'System' => [],
+    ],
+
+];
