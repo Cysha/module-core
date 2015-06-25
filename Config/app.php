7610f62bd97ff924a9b7905e440bd76b8d9c8861
xLink <xLink@cybershade.org>
2015-06-25 08:10:24 +0100

    commented up the app config

--- Config/app.php
+++ Config/app.php
@@ -1,24 +1,33 @@
 <?php
 
 return [
+    // base site
     'site-name' => 'PhoenixCMS',
-    'debugfile' => storage_path('app/debugfile'),
-    'debug' => 'false',
-    'force-secure' => 'false',
-    'minify-html' => 'false',
     'timezone' => 'UTC',
 
+    // debug
+    'debugfile' => storage_path('app/debugfile'),
+    'debug' => 'false',
+
+    // middleware
+    'force-secure' => 'false',
+    'minify-html' => 'false',
+    'cors' => 'false',
+
+    // themes
     'themes' => [
         'frontend' => 'default',
         'backend' => 'default_admin',
     ],
 
+    // site paths
     'paths' => [
         'api' => 'api/',
         'frontend' => '/',
         'backend'  => 'admin/',
     ],
 
+    // editors
     'default-editor' => 'core::editors.pagedown-bootstrap',
     'editors' => [
         'core::editors.pagedown-bootstrap',
