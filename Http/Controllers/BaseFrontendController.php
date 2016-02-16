9c659a749e3ea383418c6a1a953ea21e49209af0
xLink <xLink@cybershade.org>
2016-02-16 20:08:00 +0000

    switched up the setSidebar()

--- Http/Controllers/BaseFrontendController.php
+++ Http/Controllers/BaseFrontendController.php
@@ -9,18 +9,24 @@ class BaseFrontendController extends BaseController
      */
     public $layout = '3-column';
 
+    public $sidebar = null;
+
     public function boot()
     {
-        parent::boot();
         // spawn the menu service
         app('Cms\Modules\Core\Services\MenuService')->boot();
 
         // set the sidebar
-        $this->setSidebar();
+        if ($this->sidebar === null) {
+            $this->setSidebar('default');
+        }
+
+        parent::boot();
     }
 
     public function setSidebar($set = 'default')
     {
+        $this->sidebar = $set;
         $this->theme->setSidebar($set);
     }
 }
