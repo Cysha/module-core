16098d1004bf1a9c77f04b2475eb5dd9f023fcba
xLink <xLink@cybershade.org>
2016-02-25 15:30:16 +0000

    updated the modules blade extensions

--- Services/BladeExtender.php
+++ Services/BladeExtender.php
@@ -22,12 +22,13 @@ class BladeExtender
     }
 
     /**
-     * Add @menu support
+     * Add @menu('menu_title') support
      */
     public function addMenu($value, Application $app, Compiler $blade)
     {
         $matcher = '/@menu\s*\([\'"]([a-zA-Z0-9._-]*)[\'"]\)/';
-        return preg_replace($matcher, '<?php echo Menu::handler(\'$1\')->render(); ?> ', $value);
+        $replace = '<?php echo Menu::handler(\'$1\')->render(); ?>';
+        return preg_replace($matcher, $replace, $value);
     }
 
     /**
@@ -36,24 +37,114 @@ class BladeExtender
     public function addContinueBreak($value, Application $app, Compiler $blade)
     {
         $matcher = '/@(break|continue)/';
-        return preg_replace($matcher, '<?php $1; ?>', $value);
+        $replace = '<?php $1; ?>';
+        return preg_replace($matcher, $replace, $value);
     }
 
     /**
      * Add @set($var, 'value') support
+     * https://regex101.com/r/uD8bI1/1
      */
     public function setVar($value, Application $app, Compiler $blade)
     {
-        $matcher = '/@set\s*\(([a-zA-Z0-9\$\_\[\]\'\"]+), (.+)\)/';
-        return preg_replace($matcher, '<?php $1 = $2; ?>', $value);
+        $matcher = '/(?<!\w)(\s*)@set\s*\(\s*\${0,1}[\'\"\s]*(.*?)[\'\"\s]*,\s*([\W\w^]*?)\)$/m';
+        $replace = '$1<?php \$$2 = $3; ?>';
+        return preg_replace($matcher, $replace, $value);
     }
 
     /**
      * Add @debug($var, 'title') support
+     * https://regex101.com/r/qX1eH3/7
      */
     public function addDebug($value, Application $app, Compiler $blade)
     {
-        $matcher = '/@debug\s*\(([a-zA-Z0-9\$\_\[\]\-\>\<\)\(\'\"]+)(?:\,\s*[\"\']{1}(.+)[\"\']{1})?\)/';
-        return preg_replace($matcher, '<?php echo \Debug::dump($1, \'$2\'); ?>', $value);
+        // figure out the project root
+        $docRoot = (isset($_SERVER['DOCUMENT_ROOT']) ? $_SERVER['DOCUMENT_ROOT'] : null);
+        $docRoot = explode('/', $docRoot);
+        $docRoot = array_filter($docRoot);
+        array_pop($docRoot);
+        $docRoot = implode('/', $docRoot);
+
+        // grab the path to current blade file
+        $filePath = $blade->getPath();
+        $filePath = str_replace('\\', '/', $filePath);
+
+        // replace the project root with the blade path and boom
+        $filePath = str_replace(array($docRoot, '/~'), '~', $filePath);
+
+        $matcher = '/@debug\s*\((.*?)(?:,\s+([^)\],]+))?\)$/m';
+        $replace = sprintf('<?php echo \Debug::dump($1, \'%s\'); ?>', $filePath);
+        return preg_replace($matcher, $replace, $value);
+    }
+
+    /**
+     * Add @console($var) support
+     */
+    public function addConsole($value, Application $app, Compiler $blade)
+    {
+        $matcher = '/@console\s*\((.*?)\)/m';
+        $replace = '<?php echo \Debug::console($1); ?>';
+        return preg_replace($matcher, $replace, $value);
+    }
+
+    /**
+     * Add @authed support
+     */
+    public function addAuthed($value, Application $app, Compiler $blade)
+    {
+        $matcher = '/@authed/';
+        $replace = '<?php if (\Auth::check()): ?>';
+        return preg_replace($matcher, $replace, $value);
+    }
+
+    /**
+     * Add @authed support
+     */
+    public function addNotAuthed($value, Application $app, Compiler $blade)
+    {
+        $matcher = '/@notauthed/';
+        $replace = '<?php if (!\Auth::check()): ?>';
+        return preg_replace($matcher, $replace, $value);
+    }
+
+    /**
+     * Add @roles('admin', 'user') support
+     */
+    public function addRoles($value, Application $app, Compiler $blade)
+    {
+        $matcher = '/@roles\s*\((.*?)\)/';
+        $replace = '<?php if (\Auth::check() && \Auth::user()->hasRoles($1)): ?>';
+        return preg_replace($matcher, $replace, $value);
+    }
+
+    /**
+     * Add @role('admin') support
+     */
+    public function addRole($value, Application $app, Compiler $blade)
+    {
+        $matcher = '/@role\s*\((.*?)\)/';
+        $replace = '<?php if (\Auth::check() && \Auth::user()->hasRole($1)): ?>';
+        return preg_replace($matcher, $replace, $value);
+    }
+
+    /**
+     * Add @hasPermission('perm', 'resource'[, 'id']) support
+     * Add @permission('perm', 'resource'[, 'id']) support
+     */
+    public function addHasPermission($value, Application $app, Compiler $blade)
+    {
+        $matcher = '/@(hasPermission|permission)\s*\((.*?)\)/';
+        $replace = '<?php if (hasPermission($2)): ?>';
+        return preg_replace($matcher, $replace, $value);
+    }
+
+    /**
+     * Add a bunch of rules for endif statements
+     */
+    public function endif($value, Application $app, Compiler $blade)
+    {
+        $matcher = '/@(endroles|endrole|endauthed|endnotauthed|endhaspermission|endpermission)/i';
+        $replace = '<?php endif; ?>';
+        return preg_replace($matcher, $replace, $value);
     }
 }
