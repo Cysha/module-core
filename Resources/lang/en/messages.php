98da2f773a5e7b794a58d21da2486b90231aaa44
Dan Aldridge <xLink@cybershade.org>
2016-12-30 17:46:28 +0000

    added validation msg

--- Resources/lang/en/messages.php
+++ Resources/lang/en/messages.php
@@ -4,6 +4,7 @@ return [
 
     'errors' => [
         'csrf' => 'An error occoured whilst trying to submit the form. Please try again!',
+        'validation' => 'Validation failed! Please check the input messages and try again!',
 
     ],
 
